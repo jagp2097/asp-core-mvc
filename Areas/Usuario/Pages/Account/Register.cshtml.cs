36ea2daf0e6dd8f715707cc58fd6d99689100548
jagp2097 <jagp2097@gmail.com>
2021-03-18 23:19:55 -0600

    Video 21, propiedades, validaciones en Register.cs

--- Areas/Usuario/Pages/Account/Register.cshtml.cs
+++ Areas/Usuario/Pages/Account/Register.cshtml.cs
@@ -1,14 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace usuarios.Areas.Usuario.Pages.Account
 {
     public class RegisterModel : PageModel
     {
-        public string Message { get; set; }
-        public void OnGet(string data)
+        public void OnGet()
         {
-            Message = data;
         }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name = "Contraseña")]
+            [StringLength(100, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2}.", MinimumLength = 6)]
+            public string password { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirmar contraseña")]
+            [Compare("password", ErrorMessage = "El password no hace match")]
+            public string confirmPassword { get; set; }
+        }
+
     }
 }
